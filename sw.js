110684d0e2330e48e675333c182275ce255f9336
mmarekkamenicky-sketch <m.marek.kamenicky@gmail.com>
2026-01-08 12:44:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "ids-foto-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
+});
